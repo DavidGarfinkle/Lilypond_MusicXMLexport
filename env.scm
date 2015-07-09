a74290c40296dd5fa3fc5ea376eec8a9ca962856
admin <admin@calvisitor-10-105-148-237.CalVisitor.1918.Berkeley.EDU>
2015-07-08 17:00:00 -0700

    trying to make it a lambda (var 1 2 3 4) rather than two nested lets

--- env.scm
+++ env.scm
@@ -11,3 +11,14 @@
 			(close-output-port port)
 			(let ((port (open-input-pipe "./sxml_to_xml.guile sxml_final.scm"))) (close-pipe port)))))
 
+
+;; my attempt at changing this to a function rather than two nested lets
+(define output-file "sxml_final.scm")
+(define f-music->xml
+	((lambda (sxml port pipe obj)
+		(write `(define sxml ',sxml ) port)
+		(close-output-port port)
+		(pipe ()))
+	((music->sxml obj)
+	 (open-output-file output-file) 	
+	 (open-input-pipe (quasiquote ./sxml_to_xml.guile 	
